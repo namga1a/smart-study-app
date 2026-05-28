e947a3bf5f2be01cba24e95aaf49aa68fef78a06
Namgala <dimkanamgaladze@gmail.com>
2026-05-28 17:48:31 +0400

    davamatet Task Planner

--- js/main.js
+++ js/main.js
@@ -1,1 +1,45 @@
-console.log("Smart Study App loaded");
+const form = document.getElementById("task-form");
+const input = document.getElementById("task-input");
+const list = document.getElementById("task-list");
+
+// load from localStorage
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+function renderTasks() {
+  list.innerHTML = "";
+
+  tasks.forEach((task, index) => {
+    const li = document.createElement("li");
+
+    li.innerHTML = `
+      <span>${task}</span>
+      <button onclick="deleteTask(${index})">Delete</button>
+    `;
+
+    list.appendChild(li);
+  });
+}
+
+function addTask(e) {
+  e.preventDefault();
+
+  const value = input.value.trim();
+  if (!value) return;
+
+  tasks.push(value);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+
+  input.value = "";
+  renderTasks();
+}
+
+function deleteTask(index) {
+  tasks.splice(index, 1);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  renderTasks();
+}
+
+form.addEventListener("submit", addTask);
+
+// initial render
+renderTasks();
